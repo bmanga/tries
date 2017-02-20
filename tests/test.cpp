c59b966bb510edd460c024d761af8ff033eda0fd
Bruno Manganelli <bruno.manga95@gmail.com>
2017-02-20 23:41:55 +0000

    Added new functions and tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -21,6 +21,58 @@ static std::vector<std::wstring> wwords = {
 	L"shelter", L"fancy", L"fine"
 };
 
+TEST(utils, node_to_char_string) {
+	trie<char> t;
+	t.add("playing");
+	std::string expected = "play";
+
+	const auto *n = t.find_prefix("play");
+
+	ASSERT_EQ(node_to_string(n), expected);
+}
+
+TEST(utils, node_to_wchar_string) {
+	trie<wchar_t> t;
+	t.add(L"intermediate");
+	std::wstring expected = L"inter";
+
+	const auto *n = t.find_prefix(L"inter");
+
+	ASSERT_EQ(node_to_string(n), expected);
+}
+
+TEST(node, paths_to) {
+	trie<char> t;
+
+	t.add("italy");
+	const trie<char>::node *n = t.find_prefix("i");
+
+	trie<char>::node::path_list expected = {
+		t.find_prefix("ita")
+	};
+	ASSERT_EQ(n->paths_to('a', 0), expected);
+
+	expected = {};
+	ASSERT_EQ(n->paths_to('a', 4), expected);
+
+	t.add("iran");
+	t.add("icarus");
+
+	expected = {
+		t.find_prefix("ica"),
+		t.find_prefix("ira"),
+		t.find_prefix("ita")
+	};
+	ASSERT_EQ(n->paths_to('a'), expected);
+
+	expected.erase(begin(expected) + 1); // rm iran
+	ASSERT_EQ(n->paths_to('a', 2), expected);
+
+	expected.erase(begin(expected) + 1); // rm italy
+	ASSERT_EQ(n->paths_to('a', 3), expected);
+
+
+}
 TEST(trie, add_remove) {
 	trie<char> t;
 
@@ -45,13 +97,13 @@ TEST(trie, char_suggestions) {
 		"alike",
 		"apologise"
 	};
-	ASSERT_EQ(t.suggestions("a"), expected_a);
+	ASSERT_EQ(t.complete_suggestions("a"), expected_a);
 
 	std::vector<std::string> expected_j = {
 		"jail"
 	};
 
-	ASSERT_EQ(t.suggestions("j"), expected_j);
+	ASSERT_EQ(t.complete_suggestions("j"), expected_j);
 }
 
 TEST(trie, wchar_t_suggestions) {
@@ -64,13 +116,41 @@ TEST(trie, wchar_t_suggestions) {
 		L"alike",
 		L"apologise"
 	};
-	ASSERT_EQ(t.suggestions(L"a"), expected_a);
+	ASSERT_EQ(t.complete_suggestions(L"a"), expected_a);
 
 	std::vector<std::wstring> expected_j = {
 		L"jail"
 	};
 
-	ASSERT_EQ(t.suggestions(L"j"), expected_j);
+	ASSERT_EQ(t.complete_suggestions(L"j"), expected_j);
+}
+
+TEST(trie, closest_match) {
+	trie<char> t;
+
+	std::vector<std::string> words = {
+		"after",
+		"amo",
+		"ami",
+		"ama",
+		"exact"
+	};
+
+	for (auto &s : words) {
+		t.add(s);
+	}
+
+	// Test word length
+	ASSERT_EQ(t.closest_matches("aftert").size(), 0);
+	ASSERT_EQ(t.closest_matches("ame").size(), 3);
+
+	// Test last character typo
+	ASSERT_EQ(t.closest_matches("ame").size(), 3);
+
+	// Test second-to-last character typo
+	ASSERT_EQ(t.closest_matches("avo").size(), 1);
+
+
 }
 
 
@@ -98,6 +178,7 @@ TEST(trie, coro) {
 	ASSERT_EQ(actual, expected);
 }
 #endif
+
 
 int main(int argc, char *argv[]) {
 	testing::InitGoogleTest(&argc, argv);
--- trie.hpp
+++ trie.hpp
@@ -14,34 +14,38 @@
 
 namespace impl_
 {
-template <class ValueT>
+template <class ValueT, class DepthT, class ValueTraits>
 class node_t 
 {
+	template <class Traits>
 	struct node_less {
 		bool operator () (const node_t &lhs, const node_t &rhs) const {
-			return lhs.value() < rhs.value();
+			return Traits::lt(lhs.value(), rhs.value());
 		}
 		bool operator () (const node_t &lhs, const ValueT &rhs) const {
-			return lhs.value() < rhs;
+			return Traits::lt(lhs.value(), rhs);
 		}
 		bool operator () (const ValueT &lhs, const node_t &rhs) const {
-			return lhs < rhs.value();
+			return Traits::lt(lhs, rhs.value());
 		}
 		struct is_transparent {};
 	};
 
 public:
-	node_t(node_t *parent, ValueT ch, bool marked = false) :
-		parent_(parent), value_(ch)
+	using value_type = ValueT;
+	using depth_type = DepthT;
+	using traits_type = ValueTraits;
+	//using allocator_type = Allocator;
+
+	node_t(node_t *parent, ValueT ch, DepthT depth, bool marked = false) :
+		parent_(parent), value_(ch), depth_(depth), height_(0)
 	{
 		marked_ = marked;
-		leaf_ = true;
 	}
 
 	node_t *emplace_child(ValueT c, bool marked = false) {
-		leaf_ = false;
-
-		return const_cast<node_t*>(&*(children_.emplace(this, c, marked).first));
+		if (!height_) increase_height();
+		return const_cast<node_t*>(&*(children_.emplace(this, c, depth_ + 1, marked).first));
 	}
 
 	node_t *get_or_emplace(ValueT c) {
@@ -52,9 +56,26 @@ public:
 
 		return res;
 	}
+	using path_list = std::vector<const node_t *>;
+
+	path_list paths_to(ValueT v, unsigned min_height_req = 0) const {
+		path_list results;
+		for (const node_t &node : children_) {
+			if (const node_t *n = node.get_child(v)) {
+				if (n->height_ >= min_height_req) {
+					results.push_back(n);
+				}
+			}
+		}
+
+		return results;
+	}
 
 	void remove_child(ValueT v) {
 		children_.erase(children_.find(v));
+		if (children_.size() == 0) {
+			decrease_height();
+		}
 	}
 
 	void mark() {
@@ -70,7 +91,7 @@ public:
 	}
 
 	bool leaf() const {
-		return leaf_;
+		return height_ == 0;
 	}
 
 	ValueT value() const {
@@ -100,23 +121,84 @@ public:
 		return parent_;
 	}
 
+	DepthT depth() const {
+		return depth_;
+	}
+
 private:
-	std::set<node_t, node_less> children_;
+	void increase_height() {
+		++height_;
+		if (parent_ && parent_->height_ <= this->height_) {
+			parent_->increase_height();
+		}
+	}
+
+	void decrease_height() {
+		--height_;
+		if (parent_ && parent_->height_ >= this->height_) {
+			parent_->decrease_height();
+		}
+	}
+
+private:
+	std::set<node_t, node_less<ValueTraits>> children_;
 	node_t *parent_;
 	ValueT value_;
 
-	unsigned leaf_ : 1;
+	DepthT depth_; 
+	DepthT height_;
 	unsigned marked_ : 1;
 	unsigned collapsed_ : 1;
 };
+
+
+template <size_t MaxDepth>
+struct depth_t_selector {
+	using type = std::conditional_t <
+		MaxDepth < 256,
+		uint8_t,
+		std::conditional_t<
+		MaxDepth < 65536,
+		uint16_t,
+		uint32_t>>;
+};
 }// namespace impl_
 
-template <class CharT = char>
+
+template <class Node>
+decltype(auto) node_to_string(const Node *n, size_t extra_entries = 0) {
+	std::basic_string<
+		typename Node::value_type,
+		typename Node::traits_type> result;
+
+	size_t len = n->depth();
+	result.reserve(len + extra_entries);
+	// back_inserter maybe?
+	result.resize(len);
+	while (len-- > 0) {
+		result[len] = n->value();
+		n = n->parent();
+	}
+
+	return result;
+}
+
+
+
+/*****************************************************************************/
+
+template <class CharT = char, 
+	size_t MaxNodeDepth = 255,
+	class Traits = std::char_traits<CharT>,
+	class Allocator = std::allocator<CharT>>
 class trie
 {
 public:
-	using node = impl_::node_t<CharT>;
-	using string = std::basic_string<CharT>;
+	using node = impl_::node_t<CharT, 
+		typename impl_::depth_t_selector<MaxNodeDepth>::type,
+		Traits>;
+
+	using string = std::basic_string<CharT, Traits, Allocator>;
 
 	void add(const string &s) {
 		node *current = &root_;
@@ -129,23 +211,44 @@ public:
 		}
 	}
 
-	node *find_incomplete(const string &s) {
+	node *find_prefix(const string &s, bool closest_match = false) {
 		node *current = &root_;
 		for (size_t j = 0, len = s.length(); j < len; ++j) {
 			node *it = current->get_child(s[j]);
-			if (!it) return nullptr;
+			if (!it) return closest_match ? current : nullptr;
 			current = it;
 		}
 
 		return current;
 	}
 
-	const node *find_incomplete(const string &s) const {
-		return const_cast<trie *>(this)->find_incomplete(s);
+	const node *find_prefix(const string &s, bool closest_match = false) const {
+		return const_cast<trie *>(this)->find_prefix(s, closest_match);
 	}
 
-	std::vector<string> suggestions(const string &s) {
-		const node *it = find_incomplete(s);
+	node *find_suffix(const node *n, const string &s, bool allow_unmarked = false) {
+		if (s.empty()) return n;
+
+		string::const_iterator it = s.cbegin();
+		while (it != s.cend()) {
+			n = n->get_child(*it);
+
+			if (!n) return nullptr;
+		}
+
+		return n->marked() || allow_unmarked ? n : nullptr;
+	}
+
+	const node *find_suffix(const node *n, const string &s, bool allow_unmarked = false) const {
+		return const_cast<trie *>(this)->find_suffix(n, s, allow_unmarked);
+	}
+
+	string suggest_incomplete_fix(const std::string &s) const {
+
+	}
+
+	std::vector<string> complete_suggestions(string s) {
+		const node *it = find_prefix(s);
 		if (!it) return{};
 
 		auto sug = suggestions_impl(*it, s);
@@ -156,9 +259,64 @@ public:
 		return sug;
 	}
 
+	std::vector<string> closest_matches(string s, unsigned changes = 1) {
+		const node *it = find_prefix(s, true);
+		// Ignore any word whose first letter is incorrect.
+		if (!it) return{};
+
+		// Calculate how far away the node is from the actual string length.
+		size_t diff = s.size() - it->depth();
+
+		// Trivial case: the found node matches the string.
+		if (diff == 0) return { std::move(s) };
+
+		// We give up if the we diverge farther away than the last 2 characters.
+		if (diff > 2) return {};
+
+		std::vector<string> results;
+		if (diff == 1) {
+			for (const auto &child : it->get_children()) {
+				if (child.marked()) 
+					results.push_back(node_to_string(&child));
+			}
+
+			return results;
+		}
+
+		// We are two characters behind.
+		string last_chars{ s.end() - 2, s.end() - 1 };
+
+		auto paths_to_last = it->paths_to(*(s.end() - 1));
+
+		auto end_it = std::remove_if(begin(paths_to_last),
+			end(paths_to_last),
+			[](const auto *n) { return !(n->marked()); });
+
+		paths_to_last.erase(end_it, end(paths_to_last));
+
+		results.reserve(paths_to_last.size());
+		std::transform(begin(paths_to_last), end(paths_to_last),
+			std::back_inserter(results), [](const node *n) { return node_to_string(n); });
+
+		return results;
+	}
+
+	std::vector<string> closest_suggestions(string s) {
+		const node *it = find_prefix(s);
+		if (!it) return{};
+
+		auto sug = suggestions_impl(*it, s);
+		if (it->marked()) {
+			sug.push_back(s);
+			std::iter_swap(begin(sug), end(sug) - 1);
+		}
+		return sug;
+	}
+
+
 #ifdef EXPERIMENTAL_CORO
 	std::experimental::generator<string> lazy_suggestions(string s) const {
-		const node *it = find_incomplete(s);
+		const node *it = find_prefix(s);
 		if (!it) co_return;
 
 		if (it->marked()) {
@@ -172,7 +330,7 @@ public:
 #endif
 	
 	void remove(const string &s) {
-		node *it = find_incomplete(s);
+		node *it = find_prefix(s);
 
 		if (!it) {
 			std::cout << s << std::endl;
@@ -239,7 +397,7 @@ private:
 	}
 #endif
 private:
-	node root_{nullptr, '\0', true};
+	node root_{nullptr, '\0', 0, true};
 	size_t size_{ 0 };
-
 };
+
